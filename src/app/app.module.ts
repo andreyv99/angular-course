2f90f05cb016a8fb7d7239b1b76d7a9f61d8079d
Andrey Voronov <avoronov@hrk.langate.internal>
2019-12-20 20:31:12 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { appRoutes } from './routes';
 import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+import { EventListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
@@ -30,8 +31,23 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     Error404Component
   ],
 
-  providers: [EventService, ToastrService, EventRouteActivator],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    },
+    EventListResolver
+  ],
   bootstrap: [EventsAppComponent]
 })
 
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this event, do you really want to cancel?')
+  return true
+}
